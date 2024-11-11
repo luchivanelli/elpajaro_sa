9ea7fabe99d2a81e2600ab73795f3b472a84624f
Luciana <lucianavanelli13gmail.com>
2024-11-11 19:04:32 -0300

    commit

--- servidor/app.js
+++ servidor/app.js
@@ -49,28 +49,28 @@ app.use(
   })
 );
 
-// app.get('/', (req, res) => {
-//   pool.query('SELECT * FROM servicios', (error, respuesta) => {
-//     if (error) {
-//       return res.status(500).json({ error: 'Error en la consulta' });
-//     } 
-//     res.json(respuesta);
-//   });
-// });
-
-app.get("/", (req, res) => {
-  const htmlResponse = `
-    <html>
-      <head>
-        <title>NodeJs y Express en Vercel</title>
-      </head>
-      <body>
-        <h1>Soy un proyecto Back end en vercel</h1>
-      </body>
-    </html>
-  `;
-  res.send(htmlResponse);
+app.get('/', (req, res) => {
+  pool.query('SELECT * FROM servicios', (error, respuesta) => {
+    if (error) {
+      return res.status(500).json({ error: 'Error en la consulta' });
+    } 
+    res.json(respuesta);
+  });
 });
+
+// app.get("/", (req, res) => {
+//   const htmlResponse = `
+//     <html>
+//       <head>
+//         <title>NodeJs y Express en Vercel</title>
+//       </head>
+//       <body>
+//         <h1>Soy un proyecto Back end en vercel</h1>
+//       </body>
+//     </html>
+//   `;
+//   res.send(htmlResponse);
+// });
 
 app.get('/:id', (req, res) => {
   const { id } = req.params;
